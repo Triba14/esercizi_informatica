a95e07e85799f6ef096fab6fd2e386cb75b6bd16
triba14 <giuseppet100@gmail.com>
2024-10-26 17:48:37 +0200

    Finished fourth exercise

---
+++ fourth_exercise.c
@@ -0,0 +1,9 @@
+/* Fourth exercise*/
+#include <stdio.h>
+#define FIRST_NUMBER 264
+#define SECOND_NUMBER 36
+int main() {
+    int sum = FIRST_NUMBER + SECOND_NUMBER;
+    printf("The sum is %d\n", sum);
+
+}
