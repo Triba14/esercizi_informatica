d8efd50fa40088723fca757a89e9bc527a2a44b2
triba14 <giuseppet100@gmail.com>
2024-10-26 18:05:39 +0200

    Finished fifth exercise

---
+++ fifth_exercise.c
@@ -0,0 +1,8 @@
+/* Fifth exercise*/
+#include <stdio.h>
+#define FIRST_NUMBER 264
+#define SECOND_NUMBER 36
+int main() {
+    int subtraction = FIRST_NUMBER - SECOND_NUMBER;
+    printf("The difference is %d\n", subtraction);
+}
